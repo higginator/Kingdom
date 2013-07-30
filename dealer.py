a7227967773481ac0eb3141ae7f43be59e3ce032
Higginator <ryanhigs@gmail.com>
2013-07-30 08:34:59 -0700

    updated to do list

--- dealer.py
+++ dealer.py
@@ -15,3 +15,11 @@ class Dealer:
 	def make_clubs(self):
 		for i in range(2,11):
 			self.deck.append(Club(0,i))
+
+
+
+	def deal_cards(self, playerOne, playerTwo):
+		if (Game.current_round == 1):
+			for x in range(5):
+				PlayerOne.hand.append(deck.pop(0))
+				PlayerTwo.hand.append(deck.pop(0))
---
+++ player.py
@@ -0,0 +1,30 @@
+from cards import *
+
+class Player:
+
+	def __init__(self):
+		self.hand = []
+		self.cards_on_board = {}
+
+
+	def initialize_cards_on_board_data(self):
+		self.cards_on_board["monster1"] = {}
+		self.cards_on_board["monster2"] = {}
+		self.cards_on_board["monster3"] = {}
+		self.cards_on_board["monster1"]["heart"] = None
+		self.cards_on_board["monster1"]["club"] = None
+		self.cards_on_board["monster2"]["heart"] = None
+		self.cards_on_board["monster2"]["club"] = None
+		self.cards_on_board["monster3"]["heart"] = None
+		self.cards_on_board["monster3"]["club"] = None
+		self.cards_on_board["spade1"] = None
+		self.cards_on_board["spade2"] = None
+		self.cards_on_board["stun1"] = None
+		self.cards_on_board["stun2"] = None
+
+	def pull_card(self):
+		if len(hand) == 0:
+			hand.append(deck.pop(0))
+			hand.append(deck.pop(0))
+		else:
+			hand.append(deck.pop(0))
